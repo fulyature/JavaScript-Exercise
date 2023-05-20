130f7351e3ccba6b511ef690469646a23abe94a0
fulya_ture <fulyature@gmail.com>
2023-05-20 19:24:54 +0300

    my first exercise for js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const grade = prompt("please enter you grade");
+// if (grade >= 50) {
+//   console.log("Succeed");
+// } else if (grade < 50) {
+//   console.log("Failed");
+//
+grade >= 50 ? console.log("Succeed") : console.log("Failed");
